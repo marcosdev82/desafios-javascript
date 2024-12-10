7510f81c15aa13440079c1d19b10103061f7f337
mikek182 <marcosvinicius@geekwork.com.br>
2024-12-10 15:04:11 -0300

    Desafio -  Complete a solução para que retorne verdadeiro se o primeiro argumento (string) passado terminar com o segundo argumento (também uma string )

--- maior-numero-de-5-digitos-de-uma-serie.js
+++ maior-numero-de-5-digitos-de-uma-serie.js
@@ -12,8 +12,6 @@
 // O número será passado como uma sequência de apenas dígitos. Deve retornar um número inteiro de cinco dígitos. O número passado pode ter até 1000 dígitos.
 
 // Adaptado de ProjectEuler.net
-
-
 function solution(digits) {
     let maxNumber = 0;
     
