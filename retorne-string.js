5d165a7ff668e9989b0bb019276f258b078b3c44
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-07 14:13:52 -0300

    Retorne uma string

---
+++ retorne-string.js
@@ -0,0 +1,6 @@
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
+
+greet("Ryan") // "Hello, Ryan how are you doing today?"
+greet("Shingles") // "Hello, Shingles how are you doing today?"
