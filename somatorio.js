611c2f36e0c28dfdf9025b7ff369c813f4854492
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-09 13:25:42 -0300

    somatorio

---
+++ somatorio.js
@@ -0,0 +1,23 @@
+// Somatório
+// Escreva um programa que encontre a soma de todos os números de 1 a num (ambos inclusive). 
+// O número será sempre um inteiro positivo maior que 0. Sua função precisa retornar apenas o resultado. 
+// O que está entre parênteses no exemplo abaixo é como você chega a esse resultado, e não faz parte dele. Veja os testes de exemplo.
+
+// Por exemplo (Entrada -> Saída) :
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+
+﻿
+var summation = function (num) {
+   let result = 0;
+   for(i=1; i <= num; i++) {
+       result+=i;
+   }  
+   return result;
+}
+
+summation(1) //  1
+summation(2) //  3
+summation(8) // 36
