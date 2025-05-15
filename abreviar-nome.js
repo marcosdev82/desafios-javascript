d014eb2f994ba8605884a6cea183d8117fdaacd9
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-15 12:46:42 -0300

    Abreviar um nome de duas palavras

---
+++ abreviar-nome.js
@@ -0,0 +1,28 @@
+// Escreva uma função para converter um nome em iniciais. Este kata aceita, estritamente, duas palavras com um espaço entre elas.
+
+// A saída deve ser duas letras maiúsculas com um ponto separando-as.
+
+// Deveria ficar assim:
+
+// Sam Harris=>S.H
+
+// patrick feeney=>P.F
+
+function abbrevName(name){
+  let  nomes = name.split(' ')
+  return `${nomes[0][0]}.${nomes[1][0]}` 
+}
+
+// forma simplificada
+const abbrevName = name =>
+  name
+    .split(' ')
+    .map(n => n[0].toUpperCase())
+    .join('.');
+
+
+console.log(abbrevName("Sam Harris"));
+console.log(abbrevName("Patrick Feenan"));
+console.log(abbrevName("Evan Cole"));
+console.log(abbrevName("P Favuzzi"));
+console.log(abbrevName("David Mendieta"));
