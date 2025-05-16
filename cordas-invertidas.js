27914fef36426f9bf9c9b29af486a7712485ed97
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-16 17:02:33 -0300

    Cordas invertidas

--- cordas-invertidas.js
+++ cordas-invertidas.js
@@ -7,6 +7,14 @@ function solution(str){
     return [...str].reverse().join('')
 }
 
+// forma arcaica
+function solution(s){
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--)
+    o += s[i];
+  return o;
+}
+
 console.log(solution('world'), 'dlrow');
 console.log(solution('hello'), 'olleh');
 console.log(solution(''), '');
