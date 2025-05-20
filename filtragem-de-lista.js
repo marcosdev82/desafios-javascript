9d373b111f5d5a48848ee005829d85e55c641df4
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-20 11:38:20 -0300

    Filtragem de lista

--- filtragem-de-lista.js
+++ filtragem-de-lista.js
@@ -7,13 +7,19 @@
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
-
+// forma 1
 function filter_list(l) {
   return l.filter(function(v) {return typeof v == 'number'})
 }
 
+// forma 2
 function filter_list(l) {
  return l.filter(v => typeof v == "number")
+}
+
+// forma 3
+function filter_list(l) {
+  return l.filter(e => Number.isInteger(e));
 }
 
 filter_list([1,2,'aasf','1','123',123])
