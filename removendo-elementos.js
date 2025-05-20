2cd574a0830634e0df787bedea9b3d273bac80e0
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-20 13:22:34 -0300

    Remove elemento

---
+++ removendo-elementos.js
@@ -0,0 +1,23 @@
+// Descrição:
+// Pegue um array e remova cada segundo elemento dele. Mantenha sempre o primeiro elemento e comece a remover a partir do próximo.
+
+// Exemplo:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...]-->["Keep", "Keep", "Keep", ...]
+
+// Nenhuma das matrizes estará vazia, então você não precisa se preocupar com isso!
+
+
+function removeEveryOther(arr){
+    const newArr = []
+    for(let k in arr) {
+      if (k % 2 == 0) {
+        newArr.push(arr[k])
+      }
+    }
+    return newArr;
+}
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])) // ['Hello', 'Hello Again']
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // [1, 3, 5, 7, 9]
+console.log(removeEveryOther([[1, 2]])) // [[1, 2]]
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}])) // [['Goodbye']]
