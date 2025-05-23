2535b10945d6594ed9f1198a2cdea1ae65273ca7
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-23 14:31:53 -0300

    soma quadrada

--- soma-quadrada.js
+++ soma-quadrada.js
@@ -5,6 +5,17 @@ function squareSum(numbers){
     return numbers.reduce((acc, curr) => (curr * curr) + acc , 0);
 }
 
+
+// Outra forma
+
+function squareSum(numbers){
+  var result = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    result += (numbers[i] * numbers[i]);
+  }
+  return result;
+}
+
 console.log(squareSum([1,2]))
 console.log(squareSum([0, 3, 4, 5]))
 console.log(squareSum([]));
