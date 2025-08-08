7fe86b799f8d342e810fc5132709afc408b0b755
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-08 15:53:05 -0300

    Livro de notas

--- livro-de-notas.js
+++ livro-de-notas.js
@@ -25,3 +25,24 @@ function getGrade(s1, s2, s3) {
     }
     return num
 }
+
+getGrade(95,90,93) // 'A'
+getGrade(100,85,96) // 'A'
+getGrade(92,93,94) // 'A'
+
+getGrade(70,70,100) // 'B'
+getGrade(82,85,87) // 'B'
+getGrade(84,79,85) // 'B'
+getGrade(89,89,90) // 'B' 
+
+getGrade(70,70,70) // 'C'
+getGrade(75,70,79) // 'C'
+getGrade(60,82,76) // 'C'
+
+getGrade(65,70,59) // 'D'
+getGrade(66,62,68) // 'D'
+getGrade(58,62,70) // 'D'
+
+getGrade(44,55,52) // 'F'
+getGrade(48,55,52) // 'F'
+getGrade(58,59,60) // 'F'
